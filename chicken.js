5c5d551d82dc0f88107ff2ba93b64f0d30e5e222
BitFlipper99 <icemasta99@gmail.com>
2017-10-01 21:38:26 -0500

    fixed some things

--- chicken.js
+++ chicken.js
@@ -1,9 +1,32 @@
 
 
-function displayImage(img, x, y, width, height){
-	
 
+
+function startGame(){
+
+	var ctx = document.getElementById('canvas');
+	ctx.style.width  = '1080px';
+	ctx.style.height = '720px';
+
+
+	/*
+            if (canvas.getContext) {
+                var ctx = canvas.getContext('2d');
+
+                //Loading of the home test image - img1
+                var img1 = new Image();
+                img1.src = 'res/im/chicken_eat.png';
+
+                //drawing of the test image - img1
+                img1.onload = function () {
+                    //draw background image
+                    ctx.drawImage(img1, 0, 0);
+                    //draw a box over the top
+                    ctx.fillStyle = "rgba(0, 0, 0, 0.5)";
+                    ctx.rect(0, 0, 540, 360);
+					ctx.stroke();
+
+                };
+            }
+    */
 }
-
-
-
--- images.js
+++ images.js
@@ -1,25 +1,8 @@
-var images = [
-	{name = "chicken_eat", ext = "PNG", sprites = [32, 32, ["back0", "back1", "back2"], ["left0", "left1", "left2"], ["front0", "front1", "front2"], ["right0", "right1", "right2"]]}
+function Sprite(ctx){
 
-];
+	var that = {};
 
-for (let i = 0; i < images.length; i++) {
 
-	for (let r = 0; r < images[i].sprites.length-2; r++) {
-
-		for (let c = images[i].sprites[c+2].length + 2; p < images[i].sprites[j].length; c++){
-			new_image.id = images[i].name;
-			var new_image = document.createElement("IMG");
-			new_image.width = images[i].sprites[0];
-			new_image.height = images[i].sprites[1];
-			new_image.id = images[i].sprites[r+2][c];
-			new_image.style.background = "url(res/im/ " + images[i].name + "." + images[i].ext + ") " + r*32 + " " + c*32;
-			new_image.style.display = "none";
-			new_image.style.position = "fixed";
-			document.body.appendChild(new_image);
-		}
-
-	}
 
 }
 
