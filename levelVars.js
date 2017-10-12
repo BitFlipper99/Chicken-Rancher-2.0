48ce1ea1e98a861141415ac46383149bb0ce1153
BitFlipper99 <icemasta99@gmail.com>
2017-10-12 14:15:51 -0500

    attempting to set up dynamic graphics

--- levelVars.js
+++ levelVars.js
@@ -9,7 +9,6 @@ var
 	3: water:
 
 
-
 */
 
 level1_d = [
@@ -54,27 +53,82 @@ level2_d = [
 
 
 
+function calcDir(surr, org){
+	var eightBool = 0;
+	for (let i = 0; i < surr.length; i++){
+		eightBool << 1;
+		if (surr[i] === org) eightBool++;
+	}
+	//TEMP SWITCH CASE
+	switch(eightBool){ //me trying to figure out what sprite should display given every possibility (obviously I will make this a function once i figure it out)
+		case 0:
+			return 4;
+		case 1:
+			return 9;
+		case 2:
+			return 7;
+		case 3:
+			return 7;
+		case 4:
+			return 4;
+		case 5:
+			return 4;
+		case 6:
+			return 10;
+		case 7:
+			return ;
+		case 8:
+			return ;
+		case 9:
+			return ;
+		case 10:
+			return ;
+		case 11:
+			return ;
+		case 12:
+			return ;
+		case 13:
+			return ;
+		case 14:
+			return ;
+		case 15:
+			return ;
+		case 16:
+			return ;
+		
+	}
+
+	
+
+}
+
 function Level(data){
 	this.originalData = data;
 	//This function should convert a map to an indexed sprite map.
 	this.convertData = function(){
+		var
+		yArr = [-1, -1, -1, 0, 0, 1, 1, 1],
+		xArr = [-1, 0, 1, -1, 1, -1, 0, 1];
+
 		this.c_Data = new Array(this.originalData.length)
 		for (let i = 0; i < this.originalData.length; i++){
 			this.c_Data[i] = new Array(this.originalData[i].length);
 		}
 		for (let y = 0; y < data.length; y++){
 			for (let x = 0; x < data[y].length; x++){
+				switch(this.originalData[y][x]){
+					case 0: this.c_Data[y][x] = 12;
+						
+					case 1: this.c_Data[y][x] = 45;
+						
+					case 2: this.c_Data[y][x] = 27;
+				}
+				let surrounding = [];
+				for (let i = 0; i < 8; i++){
+					if (this.originalData[y+yArr[i]][x+xArr[i]] !== 'undefined') surrounding[i] = this.originalData[y+yArr[i]][x+xArr[i]];
+					else {surrounding[i] = this.originalData[y+yArr[i]][x+xArr[i]];}
+				}
 
-				//TODO:
-				//Implement a function that converts the original data into indexes
-
-
-
-
-
-
-
-				this.c_Data[y][x] = this.originalData[y][x] + 3;
 			}
 		}
 	}
@@ -83,7 +137,6 @@ function Level(data){
 	this.render = function(){
 		for (let y = 0; y < this.c_Data.length; y++){
 			for (let x = 0; x < this.c_Data[y].length; x++){
-				console.log("test");
 				grassSet.scale[1][this.c_Data[y][x]].draw(ctx, x*32, y*32);
 			}
 		}
@@ -136,22 +189,22 @@ function Level(data){
 				switch(fixVar){ //figure out an equation that returns a number of index to be the right graphic
 					case 0:
 
-						break;
+						
 					case 1:
 
-						break;
+						
 					case 2:
 
-						break;
+						
 					case 4:
 
-						break;
+						
 					case 8:
 
-						break;
+						
 					case 16:
 
-						break;
+						
 
 				}
 			}
@@ -159,33 +212,33 @@ function Level(data){
 				switch(temp%10){
 					case 0:
 						grassSet.scale[1].grass.foilage[foilage-1].draw(ctx, currRow, currCol);
-						break;
+						
 					case 1:
 						grassSet.scale[1].grass.foilage[foilage+3].draw(ctx, currRow, currCol);
-						break;
+						
 					case 2:
 						grassSet.scale[1].sand.foilage[foilage].draw(ctx, currRow, currCol);
-						break;
+						
 					case 3:
 						grassSet.scale[1].water.foilage[foilage%3].draw(ctx, currRow, currCol);
-						break;
+						
 				}
 			}
 			else{
 				switch(temp%10){
 					case 0:
 						grassSet.scale[1].sand.grassy[4].draw(ctx, currRow, currCol);
-						break;
+						
 
 					case 1:
 						grassSet.scale[1].grass.lite[4].draw(ctx, currRow, currCol);
-						break;
+						
 					case 2:
 						grassSet.scale[1].sand.foilage[0].draw(ctx, currRow, currCol);
-						break;
+						
 					case 3:
 						grassSet.scale[1].water.grassy[4].draw(ctx, currRow, currCol);
-						break;
+						
 				}
 			}
 
